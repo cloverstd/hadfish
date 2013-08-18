46a0b678d367bfb4fbc125a3b5dade5579eeddca
cloverstd <cloverstd@gmail.com>
2013-08-18 09:24:13 +0800

    test

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 from hadfish import app
 from hadfish.extensions import db
 from hadfish.databases import *
+from random import randint
 from datetime import datetime
 
 with app.test_request_context():
@@ -13,13 +14,13 @@ with app.test_request_context():
         # users.append(user)
 
     # db.session.add_all(users)
-    # item = ItemSale.query.get(1)
-    # items = list()
-    # for i in range(8 * 10):
-        # rv = ItemDemand(item.name, item.price, item.kind_id)
-        # rv.user_id = item.user_id
-        # rv.images = item.images
-        # items.append(rv)
+    item = ItemSale.query.get(1)
+    items = list()
+    for i in range(8 * 10):
+        rv = ItemSale(item.name, item.price, item.original_price, randint(0, 4))
+        rv.user_id = item.user_id
+        rv.images = item.images
+        items.append(rv)
 
     # db.session.add_all(items)
 
@@ -28,11 +29,11 @@ with app.test_request_context():
         # if item.id != 1:
             # db.session.delete(item)
 
-    user = User.query.filter_by(name="cloverstd").first()
-    user.power =  99
-    page = Page.query.all()
-    for p in page:
-        db.session.delete(p)
+    # user = User.query.filter_by(name="cloverstd").first()
+    # user.power =  99
+    # page = Page.query.all()
+    # for p in page:
+        # db.session.delete(p)
     db.session.commit()
 
 
