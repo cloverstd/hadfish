d270187184400cc4c7c2275b834b5bdd3a4d2681
cloverstd <cloverstd@gmail.com>
2013-08-26 10:46:02 +0800

    测试

--- test.py
+++ test.py
@@ -7,8 +7,44 @@ from hadfish.databases import *
 from random import randint
 from datetime import datetime
 from random import randint
+from hashlib import md5
+import json
+# import MySQLdb
+
+
+# con = MySQLdb.connect("localhost", "root", "xws2931336", "hadfish2", charset='utf8')
+
+# cur = con.cursor(MySQLdb.cursors.DictCursor)
+
+# cur.execute("SELECT userName, userPassword, userEmail, userFace, userQQ, userAddress, userCellphone, userRegtime FROM yy_user;")
+
+# rows = cur.fetchall()
+
+# con.close()
+users_json = None
+with open("hadfish.json") as fp:
+    users_json = json.load(fp)
+
+def import_user():
+    users = list()
+    for user in users_json:
+        users.append(User(name=user["name"],
+                          email=user["email"],
+                          password=user["password"],
+                          tel=user["tel"],
+                          qq=user["qq"],
+                          address=user["address"],
+                          avatar=user["avatar"]))
+    db.session.add_all(users)
+
 
 with app.test_request_context():
+    # import_user()
+    u = User.query.filter_by(name="cloverstd").first()
+    print u
+    u.power = 99
+
+    # me = User.query.filter_by(name="cloverstd").first()
     # users = list()
     # for i in range(100):
         # user = User("user%d" % i, "user%d@hadfish.com" % i, "1")
@@ -37,9 +73,6 @@ with app.test_request_context():
     # kinds = Kind.query.all()
     # for k in kinds:
         # print k.name, k.id
-    users = User.query.all()
-    for user in users:
-        db.session.delete(user)
     db.session.commit()
 
 
